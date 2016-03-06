dbd849c056ff650889aa5ff1022865e83c640058
Foteini Giannaropoulou <fgiannar@dotsoft.gr>
2016-03-06 11:20:13 +0200

    Add CORS

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         GrahamCampbell\Exceptions\ExceptionsServiceProvider::class,
         Heroicpixels\Filterable\FilterableServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
